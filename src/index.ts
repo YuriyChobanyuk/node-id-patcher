6338808b50a7f0b5c845a6384262e9823fe54908
Yuriy Chobanyuk <sonicmaster172@gmail.com>
2021-06-16 15:45:43 +0300

    added scanning of all templates in the folder

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,37 @@
 import fs from 'fs/promises';
-import { targetFilePath } from './contants';
+import path from 'path';
+import glob from 'glob';
+import util from 'util';
 
 import { Parser } from './parser';
+const parser = new Parser();
+const targetPath = path.join(__dirname, '../**/*.component.html');
 
-async function main(): Promise<void> {
-  const parser = new Parser();
-  const root = await parser.getFileRoot(targetFilePath);
+async function addTestIds(filePath: string): Promise<void> {
+  const root = await parser.getFileRoot(filePath);
 
   parser.collectNodesWithoutTestId(root).forEach((element) => {
     parser.appendTestId(element);
   });
-
   const newContent = root.toString();
 
-  return fs.writeFile(targetFilePath, newContent);
+  return fs.writeFile(filePath, newContent);
 }
 
-main().catch((e) => {
-  console.error(new Error('Something went wrong'));
-  console.error(e);
-});
+async function getTemplateFiles(): Promise<string[]> {
+  const promiseGlob = util.promisify(glob);
+
+  return promiseGlob(targetPath);
+}
+
+async function main(): Promise<void> {
+  const templates = await getTemplateFiles();
+  await Promise.all(
+    templates.map((templateFilePath) => addTestIds(templateFilePath))
+  );
+}
+
+// main().catch((e) => {
+//   console.error(new Error('Something went wrong'));
+//   console.error(e);
+// });
